6be119c4dd16f6d43164f807bd9ac3d58b81bc55
horseburger <34547251+horseburger@users.noreply.github.com>
2018-04-29 19:51:31 +0200

    Half of the program done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
-#include <stdio.h>
+#include <iostream.h>
+#include <string.h>
 
-extern "C" 
+extern "C" int findChar(char*, char*);
 
-int main(){
+char* rn = "\n\r$";
 
+
+
+void main(){
+	char STRING[64];
+	cin.getline(STRING,59);
+	strcat(STRING,rn);
+	char c[1];
+	cin >> c[0];
+	// printf("%c",c);
+	int x = findChar(STRING,c);
+	cout << x;
 }
--- modules.asm
+++ modules.asm
@@ -1,12 +1,46 @@
 		MODEL SMALL, C
+		PUBLIC findChar
 
 		.DATA
 
+char 	DB 		(?)
+
 		.CODE
 
+ShowStr		MACRO
+			mov ah, 09H
+			int 21H
+			ENDM
+
 findChar PROC
+		
+		int 	3h
+		push 	BP
+		mov 	BP, SP
 
+		mov 	si, [BP+6]
+		mov 	bh, [si]
+		mov 	char, bh
 
-ENDP
+		mov 	si, [BP+4]
+		mov 	ch, [si]
+		xor 	ax, ax
+findLoop:
 
-END
+		cmp 	ch, bh
+		jne 	notEqual
+		inc 	ax
+notEqual:
+		inc 	si
+		mov 	ch, [si]
+		cmp 	ch, 0Ah
+		je 		koniec
+		jmp 	findLoop
+
+koniec:
+
+		pop 	BP
+		ret
+		ENDP
+
+		END
