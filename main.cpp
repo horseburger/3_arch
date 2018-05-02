46669675f2cc4727a6a5f6d57cf17b9c42c8c0ec
horseburger <34547251+horseburger@users.noreply.github.com>
2018-05-02 12:39:40 +0200

    Finished program

--- main.cpp
+++ main.cpp
@@ -1,19 +1,35 @@
 #include <iostream.h>
+#include <stdio.h>
 #include <string.h>
 
 extern "C" int findChar(char*, char*);
+extern "C" float arrayAvg(float*, int);
 
 char* rn = "\n\r$";
 
 
 
 void main(){
+
+	int n = 3;
+	float array[3];
+	for(int i=0;i<n;i++){
+		cin >> array[i];
+	}
+	float avg = arrayAvg(array, n);
+	cout << "Srednia wartosc tablicy to: " << avg << "\n";
+
+	cin.clear();
+	cin.sync();
+
 	char STRING[64];
+	cout << "Podaj string: ";
 	cin.getline(STRING,59);
 	strcat(STRING,rn);
 	char c[1];
+	cout << "Podaj char: ";
 	cin >> c[0];
-	// printf("%c",c);
 	int x = findChar(STRING,c);
-	cout << x;
+	printf("Ilosc wystapien '%c' to: %i",c[0],x);
+
 }
--- modules.asm
+++ modules.asm
@@ -1,9 +1,11 @@
 		MODEL SMALL, C
 		PUBLIC findChar
+		PUBLIC arrayAvg
+		.386P
 
 		.DATA
 
-char 	DB 		(?)
+charsum 	DW 		(?)
 
 		.CODE
 
@@ -11,6 +13,32 @@ ShowStr		MACRO
 			mov ah, 09H
 			int 21H
 			ENDM
+
+arrayAvg	PROC
+		
+		int 	3h
+		push 	BP
+		mov 	BP, SP
+		sub 	SP, 4
+
+		mov 	CX, [BP+6]
+		mov 	word ptr [bp-4], CX
+		mov 	word ptr [bp-2], 0
+
+		mov 	bx, [BP+4]
+		fldz
+
+addLoop:
+		fadd 	dword ptr [bx]
+		add 	bx, 4
+		loop 	addLoop
+		fidiv 	dword ptr [bp-4]
+
+
+		add 	SP, 4
+		pop 	BP
+		ret
+		ENDP
 
 findChar PROC
 		
@@ -20,7 +48,6 @@ findChar PROC
 
 		mov 	si, [BP+6]
 		mov 	bh, [si]
-		mov 	char, bh
 
 		mov 	si, [BP+4]
 		mov 	ch, [si]
@@ -29,7 +56,7 @@ findLoop:
 
 		cmp 	ch, bh
 		jne 	notEqual
-		inc 	ax
+		inc 	charsum
 notEqual:
 		inc 	si
 		mov 	ch, [si]
@@ -39,6 +66,7 @@ notEqual:
 
 koniec:
 
+		mov 	ax, charsum
 		pop 	BP
 		ret
 		ENDP
